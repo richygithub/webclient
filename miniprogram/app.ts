e3595ca0705baca169b0c4c49446ae692bdd94f5
libl <libl@mail.jj.cn>
2025-03-09 09:27:49 +0800

    m

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -3,7 +3,16 @@ App<IAppOption>({
   globalData: {
     apiBase:"https://127.0.0.1:443"
   },
-  onLaunch() {
+  onLaunch(options) {
+
+    if (options.query.scene) { // 处理扫码场景
+      console.log("scan code.",options.query.scene)
+      const scene = decodeURIComponent(options.query.scene)
+      wx.navigateTo({
+        url: `/pages/product-detail/product-detail?id=${scene}`
+      })
+    }
+
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
--- miniprogram/pages/product-detail/product-detail.ts
+++ miniprogram/pages/product-detail/product-detail.ts
@@ -10,11 +10,15 @@ interface CourseInfo {
   id: string
   title: string
   price: number
+  cover: string
   desc: string
   images: string[]
   duration: string
   location: string
   features: string[]
+}
+interface CourseCode{
+  data :ArrayBuffer
 }
 // 模拟数据生成器
 const mockCourseData = (id: string): CourseInfo => ({
@@ -22,6 +26,7 @@ const mockCourseData = (id: string): CourseInfo => ({
   title: '沉浸式自然探索课程',
   price: 298,
   desc: '这是一门专为青少年设计的户外探索课程，通过实地观察、动手实践和团队协作，帮助学员深入了解生态系统。课程包含森林徒步、动植物识别、野外生存技能等内容。',
+  cover:'沉浸式自然探索课程',
   images: [
     'https://picsum.photos/750/500?random=1',
     'https://picsum.photos/750/500?random=2',
@@ -46,14 +51,8 @@ Page({
   },
 
   onLoad(options: { id: string }) {
-    //this.loadCourseDetail(options.id)
-        // 模拟延迟加载
-        setTimeout(() => {
-          this.setData({
-            course: mockCourseData(options.id || 'mock_001'),
-            loading: false
-          })
-        }, 800) // 0.8秒模拟网络延迟
+    this.loadCourseDetail(options.id)
+    this.loadQrcode(options.id)
   },
 
   async loadCourseDetail(id: string) {
@@ -76,6 +75,39 @@ Page({
       },
     })
 
+  },
+  // 获取二维码
+  async loadQrcode(courseId: string) {
+    wx.showLoading({ title: '生成二维码中' })
+  
+    try {
+       wx.request<ArrayBuffer>({
+        url: `${getApp().globalData.apiBase}/courses/${courseId}/qrcode`,
+        responseType: 'arraybuffer',
+        method: 'GET',
+        success: (res) => {
+          console.log('请求成功:', res.data); // res.data为服务器响应内容
+          const base64 = wx.arrayBufferToBase64(res.data)
+          this.setData({
+            qrcodeUrl: 'data:image/png;base64,' + base64
+          })
+        },
+      })
+
+      // const res = awaitwx.request({
+      //   url: `${getApp().globalData.apiBase}/courses/${courseId}/qrcode`,
+      //   responseType: 'arraybuffer'
+      // })
+      
+      // const base64 = wx.arrayBufferToBase64(res.data)
+      // this.setData({
+      //   qrcodeUrl: 'data:image/png;base64,' + base64
+      // })
+    } catch (err) {
+      wx.showToast({ title: '二维码加载失败', icon: 'none' })
+    }
+    
+    wx.hideLoading()
   },
 
   handleError() {
